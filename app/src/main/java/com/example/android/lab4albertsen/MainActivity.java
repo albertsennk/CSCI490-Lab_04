7ee3c160810cb4adca38f91bbe7b057d04cf1ee7
Noah Albertsen <albertsennk@g.cofc.edu>
2019-03-04 18:34:52 -0500

    url connection lab

--- app/src/main/java/com/example/android/lab4albertsen/MainActivity.java
+++ app/src/main/java/com/example/android/lab4albertsen/MainActivity.java
@@ -2,12 +2,72 @@ package com.example.android.lab4albertsen;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class MainActivity extends AppCompatActivity {
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        webConnect("https://jsonplaceholder.typicode.com/users");
+
+//        text = findViewById(R.id.text);
+//        String ok
+    }
+
+    public String webConnect(String... string) {
+        URL url = null;
+        try {
+            url = new URL(string[0]);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        URLConnection connection = null;
+        try {
+            connection = url.openConnection();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        InputStreamReader inputStreamReader = null;
+        try {
+            inputStreamReader = new InputStreamReader(connection.getInputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+        String line =null;
+
+        try {
+            line = bufferedReader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        int i = 0;
+        while(i < 20) {
+            try {
+                line += bufferedReader.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            i++;
+        }
+
+        return line;
     }
 }
